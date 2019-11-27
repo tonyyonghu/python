36c32df8f267cc5798ce613b6c876c7175deea3a
tonyyonghu <1154771388@qq.com>
2019-11-27 23:53:41 +0800

    requests用法和字符串格式化

--- use_requests.py
+++ use_requests.py
@@ -25,6 +25,53 @@ def get_html(url):
 	# print(response.text)
 
 url="http://search.dangdang.com/?key=python&act=input"
-get_html(url)
-get_book()
+# get_html(url)
+# get_book()
 #http://search.dangdang.com/?key=python&act=input
+#用%格式化
+def format_str():
+	name="张三"
+	print("%s" % name)
+	num=23.3444
+	print("%.3f" % num)
+	num=34
+	print("%04d" % num)
+	tt=(1,2,3,4,5,5,6,7)
+	print("%s" % str(tt))
+
+format_str()
+
+def format_str_2():
+	#使用位置
+	print("欢迎您,{0},{1},''''{1}说".format('张三',"好久不仅"))
+	#使用名称
+	print("您好，{username},您的编号是{num}".format(username="张三",num=54))
+
+	data={
+		'username':'历史',
+		'num':87
+	}
+	print("您好，{username},您的编号是{num}".format(**data))
+
+	#格式化元组
+	point=(3,4,5,6,7,9)
+	print("位置坐标：{0[0]}:{0[1]}".format(point))
+
+	#格式化类
+	user=User("历史",98)
+	# print(user.show())
+	print(user)
+
+class User(object):
+	def __init__(self,username,age):
+		self.username = username
+		self.age = age
+
+	def show(self):
+		return "用户名：{self.username},年龄{self.age}".format(self=self)
+
+	def __str__(self):
+		return self.show()
+format_str_2()
+
+# https://www.bilibili.com/video/av68337879?p=9
